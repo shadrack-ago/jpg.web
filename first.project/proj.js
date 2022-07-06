e34d5614d38742ca184cc985bdb178e38a9cc5f8
shadrack-ago <100674408+shadrack-ago@users.noreply.github.com>
2022-07-06 09:01:04 +0300

    Add files via upload

---
+++ first.project/proj.js
@@ -0,0 +1,32 @@
+/*console.log('hello shady');
+       //  alert('incorrect password');
+       function exam(name,regNo)
+       {
+        let name= prompt('enter your name');
+        let regNo= prompt ('enter your registration number');
+        var results;
+        results=('B'+'name'+'regNo');
+        //console.log(results);
+
+       }
+       exam();
+       results=('B'+'name'+'regNo');
+       console.log(results);
+ //loops
+ */
+ var num=1;
+ while (num<99);
+num++;
+
+console.log(num);
+
+
+let fruits = 'mango';
+console.log(fruit.length);
+console.log(fruit.indexof('ma'));
+console.log(fruit.replace('ma',123));
+console.log(fruit.toLowercase n);
+
+
+
+
